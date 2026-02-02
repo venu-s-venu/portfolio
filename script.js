8ea7b5dd27468cce94fa8694927f8d6a833d0091
venu-s-venu <225722108+venu-s-venu@users.noreply.github.com>
2026-02-02 18:20:14 +0530

    updated pages

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+  function openModal() {
+    document.getElementById("modal").style.display = "flex";
+  }
+
+  function closeModal() {
+    document.getElementById("modal").style.display = "none";
+  }
