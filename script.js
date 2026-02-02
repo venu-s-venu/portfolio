0ff45880168b1c672b93db5f639f62c53fafa5a3
venu-s-venu <225722108+venu-s-venu@users.noreply.github.com>
2026-02-02 21:48:06 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function toggleDarkMode() {
+function DarkMode() {
   document.body.classList.toggle("dark-mode");
 }
-  function openModal() {
+function openModal() {
     document.getElementById("modal").style.display = "flex";
   }
 
-  function closeModal() {
+function closeModal() {
     document.getElementById("modal").style.display = "none";
   }
+function contact(){
+  document.getElementById("contact").style.display="flex";
+}
